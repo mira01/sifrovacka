2a03e1505bd78b0243e9594cad24636beb13d905
Miroslav Čech <miroslav.cech@gooddata.com>
2020-03-21 15:11:29 +0100

    verification of requests and logging

--- src/game_webhook.erl
+++ src/game_webhook.erl
@@ -6,13 +6,23 @@
 -include_lib("yaws/include/yaws_api.hrl").
 -include("game.hrl").
 
-out(#arg{req = Req, clidata = Clidata} = Arg) ->
+out(#arg{req = #http_request{method='GET'} = Req, querydata = Q} = Arg) ->
+    Params = uri_string:dissect_query(Q),
+    "subscribe" = proplists:get_value("hub.mode", Params),
+    "lhota_trophy" = proplists:get_value("hub.verify_token", Params),
+    Challenge = proplists:get_value("hub.challenge", Params),
+    [{status, 200},
+     {content, "text/plain", Challenge}];
+
+out(#arg{req = Req, clidata = Clidata, querydata = Q} = Arg) ->
+    io:format("Req data: ~p~n", [Clidata]),
     %{abs_path, AbsPath} = (Arg#arg.req)#http_request.path,
     Events = game_event_parser:parse_event(Clidata),
     Resp = lists:foldl(fun(E, Resps) ->
                                R = process_event(E),
                                Resps ++ R
                        end, [], Events),
+    io:format("Resp: ~p~n", [Resp]),
     [{status, 200},
      {content, "text/plain", io_lib:format("~p~n", [Resp])}].
 
