543b696340e85c6060e5b71a3c6cc7b13b503dcd
Miroslav Čech <miracech@email.cz>
2024-06-05 00:35:15 +0200

    remove unnecessary unicode functions

--- messenger/msgr/src/msgr.erl
+++ messenger/msgr/src/msgr.erl
@@ -124,13 +124,13 @@ mime(image, "jpg") ->
     mime(image, "jpeg");
 mime(Type, Extension) ->
     LowerExtension = string:lowercase(Extension),
-    StartBoundary = unicode:characters_to_binary([atom_to_list(Type), "/", LowerExtension]).
+    StartBoundary = erlang:iolist_to_binary([atom_to_list(Type), "/", LowerExtension]).
 
 format_multipart(TextData, BinaryData, Boundary) ->
-    StartBoundary = unicode:characters_to_binary([<<"--">>, Boundary]),
+    StartBoundary = erlang:iolist_to_binary([<<"--">>, Boundary]),
     LineSeparator = <<"\r\n">>,
     TextParts = lists:foldl(fun({Key, Value}, Acc) ->
-        unicode:characters_to_binary([
+        erlang:iolist_to_binary([
             Acc,
             StartBoundary, LineSeparator,
             <<"Content-Disposition: form-data; name=\"">>, Key, <<"\"">>, LineSeparator, LineSeparator,
@@ -138,7 +138,7 @@ format_multipart(TextData, BinaryData, Boundary) ->
         ])
     end, <<"">>, TextData),
     AllParts = lists:foldl(fun({Key, Binary, Mime, Filename}, Acc) ->
-        unicode:characters_to_binary([
+        erlang:iolist_to_binary([
             Acc,
             StartBoundary, LineSeparator,
             <<"Content-Disposition: form-data; name=\"">>, Key, <<"\"; filename=\"">>, Filename, <<"\"">>, LineSeparator,
@@ -147,4 +147,4 @@ format_multipart(TextData, BinaryData, Boundary) ->
             LineSeparator
         ])
     end, TextParts, BinaryData),
-    unicode:characters_to_binary([AllParts, StartBoundary, <<"--">>, LineSeparator]).
+    erlang:iolist_to_binary([AllParts, StartBoundary, <<"--">>, LineSeparator]).
